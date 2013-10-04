60d86719643fddca0a1e6973c1d5dcd3ecb24509
vintagegamingsystems <vintagegamingsystems@gmail.com>
2013-10-04 13:55:36 -0700

    Create diskdisc.ps1

---
+++ diskdisc.ps1
@@ -0,0 +1,52 @@
+# Thanks to Tobias who created "Get Drive ID and Drive Leter" for which I modified to return the correct JSON output.
+# http://powershell.com/cs/media/p/7924.aspx
+
+function Combine-Object { 
+        param( 
+        $object1, 
+        $object2 
+        ) 
+     
+        trap { 
+            $a = 1 
+            continue 
+        } 
+        $propertylistObj1 = @($object1 | Get-Member -ea Stop -memberType *Property | Select-Object -ExpandProperty Name) 
+        $propertylistObj2 = @($object2 | Get-Member -memberType *Property | Select-Object -ExpandProperty Name | Where-Object { $_ -notlike '__*'}) 
+     
+        $propertylistObj2 | ForEach-Object { 
+            if ($propertyListObj1 -contains $_) { 
+                $name = '_{0}' -f $_ 
+            } else { 
+                $name = $_ 
+            } 
+     
+            $object1 = $object1 | Add-Member NoteProperty $name ($object2.$_) -PassThru 
+        } 
+     
+        $object1 
+    }  
+     
+    function Get-Drives { 
+        Get-WmiObject Win32_DiskPartition | 
+        ForEach-Object { 
+            $partition = $_ 
+            $logicaldisk = $partition.psbase.GetRelated('Win32_LogicalDisk') 
+            if ($logicaldisk -ne $null) {   
+	    Combine-Object $logicaldisk $partition 
+	    } 
+        } | select-Object Name, DiskIndex
+    } 
+     
+    	$colItems = Get-Drives 
+    	write-host "{"
+	write-host " `"data`":["
+	foreach ($objItem in $colItems)
+		{
+		$line= "{ `"{#DISKNUMLET}`" : `"" + $objItem.DiskIndex + " " + $objItem.Name + "`" },"
+		write-host $line
+		}
+	write-host
+	write-host " ]"
+	write-host "}"
+	write-host
