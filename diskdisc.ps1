e1f3a72abe3c9f96ab7bfac58c0075b330d1d8be
vintagegamingsystems <vintagegamingsystems@gmail.com>
2013-10-14 15:40:16 -0700

    Update diskdisc.ps1

--- diskdisc.ps1
+++ diskdisc.ps1
@@ -1,6 +1,4 @@
 $drives = typeperf -qx physicaldisk | findstr /r "Idle" | select-string -pattern "[0-9]\s[a-z]:((\s[a-z]:)+(\s)?)?" -allmatches -list | select matches
-#write-host $drives
-#[0-9]\s[a-z]:((\s[a-z]:)+)?
 write-host "{"
 	write-host " `"data`":[`n"
 	foreach ($perfDrives in $drives)
