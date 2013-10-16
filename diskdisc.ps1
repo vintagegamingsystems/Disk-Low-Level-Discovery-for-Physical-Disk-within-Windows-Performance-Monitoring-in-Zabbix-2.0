2499f93fac86b607f3fe9e9b7ed8635c5b858b2a
vintagegamingsystems <vintagegamingsystems@gmail.com>
2013-10-16 11:42:50 -0700

    Update diskdisc.ps1

--- diskdisc.ps1
+++ diskdisc.ps1
@@ -1,10 +1,10 @@
 #$drives = typeperf -qx physicaldisk | findstr /r "Idle" | select-string -pattern "[0-9]\s[a-z]:((\s[a-z]:)+(\s)?)?" -allmatches -list | select matches
-$drives = Get-WmiObject win32_PerfFormattedData_PerfDisk_PhysicalDisk |?{$_.name -ne "_Total"} |ft Name, PercentIdleTime –autosize
+$drives = Get-WmiObject win32_PerfFormattedData_PerfDisk_PhysicalDisk |?{$_.name -ne "_Total"} | Select Name
 write-host "{"
 	write-host " `"data`":[`n"
 	foreach ($perfDrives in $drives)
 		{
-		$line= "{ `"{#DISKNUMLET}`" : `"" + $perfDrives.name + "`" },"
+		$line= "{ `"{#DISKNUMLET}`" : `"" + $perfDrives.Name + "`" },"
 		write-host $line
 		}
 	write-host
